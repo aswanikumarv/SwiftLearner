04b57b506f56d14b6d149e3468072dcc70a0a40e
aswanikumarv <aswanikumar_v@outlook.com>
2016-02-14 12:05:39 +0530

    first commit

---
+++ SwiftLearner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLearner
+//
+//  Created by Aswani Kumar V on 14/02/16.
+//  Copyright © 2016 Aswani Kumar V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
